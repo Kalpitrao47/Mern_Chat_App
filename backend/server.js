2d8c4f27db2451caf5cc2b67d65692366d7429fb
KalpitRao47 <kalpitraorane47@gmail.com>
2024-03-26 17:53:03 +0530

    backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -9,16 +9,21 @@ app.get("/", (req, res) => {
     // root route http://localhost:5000
     res.send("Hello World!!!");
 });
-app.get("/api/auth/signup", (req, res) => {
-    console.log("SignUp Route")    
-});
+// app.get("/api/auth/signup", (req, res) => {
+//     console.log("SignUp Route")    
+// });
 
-app.get("/api/auth/login", (req, res) => {
-    console.log("Login Route")
-});
+// app.get("/api/auth/login", (req, res) => {
+//     console.log("Login Route")
+// });
 
-app.get("/api/auth/logout", (req, res) => {
-    console.log("Logout Route")
-});
+// app.get("/api/auth/logout", (req, res) => {
+//     console.log("Logout Route")
+// });
+//We will use middleware routes so that we wont make this ugly storing all of the routes in the same server.js file.
+
+app.use("/api/auth", authroutes)
+
+
 
 app.listen(PORT, () => console.log(`Server running on port${PORT}`));
