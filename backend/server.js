7898fea11c99da9e18ee564158321388fb4fef5c
KalpitRao47 <kalpitraorane47@gmail.com>
2024-03-26 13:43:04 +0530

    backend

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,24 @@
-const express = require("express"); 
-
+// const express = require("express"); 
+import express from "express";
+// const dotenv = require("dotenv"); 
+import dotenv from "dotenv"
 const app = express();
-
+dotenv.config();
+const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
     // root route http://localhost:5000
     res.send("Hello World!!!");
 });
+app.get("/api/auth/signup", (req, res) => {
+    console.log("SignUp Route")    
+});
 
-app.listen(5000, () => console.log('Server running on port 5000'));
+app.get("/api/auth/login", (req, res) => {
+    console.log("Login Route")
+});
+
+app.get("/api/auth/logout", (req, res) => {
+    console.log("Logout Route")
+});
+
+app.listen(PORT, () => console.log(`Server running on port${PORT}`));
