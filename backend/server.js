336bf9c2e2e844481e2b06783cebca483557f3ce
KalpitRao47 <kalpitraorane47@gmail.com>
2024-03-28 15:20:40 +0530

    Backend

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
-import authroutes from "./routes/auth.routes.js";
+// console.log("Hello world14") 
+import authroutes from "./routes/auth.routes.js"
 // const express = require("express"); 
 import express from "express";
 // const dotenv = require("dotenv"); 
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+
 const app = express();
+
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
@@ -23,8 +26,6 @@ app.get("/", (req, res) => {
 // });
 //We will use middleware routes so that we wont make this ugly storing all of the routes in the same server.js file.
 
-app.use("/api/auth", authroutes);
-
-
+app.use("/api/auth", authroutes); 
 
 app.listen(PORT, () => console.log(`Server running on port${PORT}`));
